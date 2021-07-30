0fce9b062ce0e1d4fd331c8a6853cf3e95bef19d
ZMAN <etta1055@gmail.com>
2021-07-30 10:31:30 -0500

    range and float

--- basics.py
+++ basics.py
@@ -1,5 +1,26 @@
-day_hours = 24
-week_days = 7
+# day_hours = 24
+# week_days = 7
 
-week_hours = day_hours * week_days
-print(week_hours)
+# week_hours = day_hours * week_days
+# print(week_hours)
+
+# different types integer, string, float
+# x = 10
+# y = '10'
+# z = 10.1
+
+# print(type(x), type(y), type(z))
+
+# adding numbers together and printing out
+# x = 10
+# y = 25
+# z = 25
+# s = x + y + z
+
+# print(s)
+
+# student_grades = [9.1, 8.8, 7.5]
+
+# RANGE CREATES A LIST OF NUMBERS
+# list(range(1,10)) will output [1,2,3,4,5,6,7,8,9]
+#list(range(1, 10, 2)) will output [1,3,5,7,9] counts happens every two items starting at one and ending at 9
