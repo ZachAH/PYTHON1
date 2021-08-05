32e55d6bdd9dcd0531bca02fcb63a9f6d00e8148
ZMAN <etta1055@gmail.com>
2021-08-05 10:28:53 -0500

    notes

--- first_program.py
+++ first_program.py
@@ -1,3 +1,9 @@
+# defining function taking in a phrase assigining variable then capitalizing the first letter of the phrase making capitalized phrase 
+
+# if phrase starts with interrogatives then it will return with the first letter capitalized other wise it will return with capitalized without question mark
+
+# if the user types in /end it will end the loop and print out what the user input stored in the empty list results 
+
 def sentence_maker(phrase):
     interrogatives = ("how", "what", "why")
     capitalized = phrase.capitalize()
