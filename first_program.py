fdedd868cbb22c183ee1b5faef96cbdb65e05079
ZMAN <etta1055@gmail.com>
2021-08-11 09:32:42 -0500

    function

--- first_program.py
+++ first_program.py
@@ -4,22 +4,38 @@
 
 # if the user types in /end it will end the loop and print out what the user input stored in the empty list results 
 
-def sentence_maker(phrase):
-    interrogatives = ("how", "what", "why")
-    capitalized = phrase.capitalize()
-    if phrase.startswith(interrogatives):
-        return "{}?".format(capitalized)
-    else:
-        return "{}.".format(capitalized)
+# def sentence_maker(phrase):
+#     interrogatives = ("how", "what", "why")
+#     capitalized = phrase.capitalize()
+#     if phrase.startswith(interrogatives):
+#         return "{}?".format(capitalized)
+#     else:
+#         return "{}.".format(capitalized)
 
-results = []
+# results = []
 
-while True:
-    user_input = input("Say something: ")   
-    if user_input == "/end":
-        break
-    else:
-        results.append(sentence_maker(user_input))
+# while True:
+#     user_input = input("Say something: ")   
+#     if user_input == "/end":
+#         break
+#     else:
+#         results.append(sentence_maker(user_input))
 
-print(" ".join(results))        
+# print(" ".join(results))        
 
+# temps = [221, 234, 340, -9999, 230]
+
+# new_temps = [temp / 10 if temp != -9999 else 0 for temp in temps]
+
+# print(new_temps)
+
+# function that takes parameter a list that contains both numbers and strings if item in list is a string it wil return zero if item is int it will return the vaule
+
+# def foo(lst):
+#     return [i if not isinstance(i, str)
+#         else 0 for i in lst ]
+
+
+
+# print(foo(['zach', 89, 'rachel', 89, 67])
+# )
